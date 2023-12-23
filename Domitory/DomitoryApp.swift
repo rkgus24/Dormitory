4bfe0d80952a4af0a3760b2bcf825f6f7cc320f2
rkgus24 <shrkgus1124@gmail.com>
2023-12-23 15:14:24 +0900

    Initial Commit

---
+++ Domitory/DomitoryApp.swift
@@ -0,0 +1,32 @@
+//
+//  DomitoryApp.swift
+//  Domitory
+//
+//  Created by 노가현 on 12/23/23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct DomitoryApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
