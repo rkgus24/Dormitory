3e5ef1aa27a61fa988f24dbf19758145e20e29c3
rkgus24 <shrkgus1124@gmail.com>
2023-12-26 08:53:23 +0900

    Dormitory-first

---
+++ Domitory/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  Domitory
+//
+//  Created by 노가현 on 12/25/23.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
